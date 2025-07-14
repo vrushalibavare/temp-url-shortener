dc21a1179aba46993df2242b0e2c830059bafdeb
vrushalibavare <vrushali.bavare@gmail.com>
2025-07-14 17:21:08 +0800

    added changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,20 +84,30 @@ resource "aws_api_gateway_resource" "newurl" {
   path_part   = "newurl"
 }
 
-resource "aws_api_gateway_method" "post" {
+resource "aws_api_gateway_method" "post_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.newurl.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "post" {
+resource "aws_api_gateway_integration" "post_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.newurl.id
-  http_method             = aws_api_gateway_method.post.http_method
+  http_method             = aws_api_gateway_method.post_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.create_url.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.newurl.id
+  http_method = aws_api_gateway_method.post_method.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
 }
 
 resource "aws_lambda_permission" "api_gateway_create" {
@@ -108,27 +118,59 @@ resource "aws_lambda_permission" "api_gateway_create" {
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
 
-resource "aws_api_gateway_resource" "shortid" {
+resource "aws_api_gateway_resource" "geturl" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   path_part   = "{short_id}"
 }
 
-resource "aws_api_gateway_method" "get" {
+resource "aws_api_gateway_method" "get_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.shortid.id
+  resource_id   = aws_api_gateway_resource.geturl.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "get" {
+resource "aws_api_gateway_integration" "get_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.shortid.id
-  http_method             = aws_api_gateway_method.get.http_method
+  resource_id             = aws_api_gateway_resource.geturl.id
+  http_method             = aws_api_gateway_method.get_method.http_method
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  type                    = "AWS"
   uri                     = aws_lambda_function.retrieve_url.invoke_arn
+  request_templates = {
+    "application/json" = <<EOF
+    { 
+      "short_id": "$input.params('short_id')" 
+    }
+EOF
+  }
 }
+
+resource "aws_api_gateway_method_response" "response_302" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.geturl.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = "302"
+  response_parameters = {
+    "method.response.header.Location" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "get_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.geturl.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = aws_api_gateway_method_response.response_302.status_code
+  response_parameters = {
+    "method.response.header.Location" = "integration.response.body.location"
+  }
+  depends_on = [
+    aws_api_gateway_integration.get_integration
+  ]
+}
+
+
 
 resource "aws_lambda_permission" "api_gateway_retrieve" {
   statement_id  = "AllowExecutionFromAPIGateway"
@@ -139,8 +181,31 @@ resource "aws_lambda_permission" "api_gateway_retrieve" {
 }
 
 resource "aws_api_gateway_deployment" "api" {
-  depends_on  = [aws_api_gateway_integration.post, aws_api_gateway_integration.get]
+  depends_on = [
+    aws_api_gateway_integration.post_integration,
+    aws_api_gateway_integration.get_integration,
+    aws_api_gateway_method_response.response_200,
+    aws_api_gateway_method_response.response_302,
+    aws_api_gateway_integration_response.get_integration_response
+  ]
   rest_api_id = aws_api_gateway_rest_api.api.id
+  
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.newurl.id,
+      aws_api_gateway_method.post_method.id,
+      aws_api_gateway_integration.post_integration.id,
+      aws_api_gateway_resource.geturl.id,
+      aws_api_gateway_method.get_method.id,
+      aws_api_gateway_integration.get_integration.id,
+      aws_api_gateway_method_response.response_302.id,
+      aws_api_gateway_integration_response.get_integration_response.id
+    ]))
+  }
+  
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_api_gateway_stage" "prod" {
@@ -151,17 +216,41 @@ resource "aws_api_gateway_stage" "prod" {
 
 
 
-# Use existing certificate from ap-southeast-1
-data "aws_acm_certificate" "existing_cert" {
-  domain   = var.domain_name
-  statuses = ["ISSUED"]
+# Create ACM certificate
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
+# Route 53 validation record
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  zone_id = var.hosted_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
 
+# Certificate validation
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
 
+# API Gateway custom domain
 resource "aws_api_gateway_domain_name" "custom" {
   domain_name              = var.domain_name
-  regional_certificate_arn = data.aws_acm_certificate.existing_cert.arn
+  regional_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
   endpoint_configuration {
     types = ["REGIONAL"]
   }
